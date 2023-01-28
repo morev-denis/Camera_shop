57a872094f97daaf373c75b22ff60a5dd67c4985
Denis Morev <morev.denis@gmail.com>
2023-01-28 11:55:25 +0300

    создать компонент Banner

--- src/pages/catalog-screen/catalog-screen.tsx
+++ src/pages/catalog-screen/catalog-screen.tsx
@@ -1,4 +1,5 @@
 import Header from '../../components/header/header';
+import Banner from '../../components/banner/banner';
 import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import CatalogFilter from '../../components/catalog-filter/catalog-filter';
 import CatalogSort from '../../components/catalog-sort/catalog-sort';
@@ -11,31 +12,7 @@ const CatalogScreen = () => (
     <Header />
 
     <main>
-      <div className="banner">
-        <picture>
-          <source
-            type="image/webp"
-            srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-          />
-          <img
-            src="img/content/banner-bg.jpg"
-            srcSet="img/content/banner-bg@2x.jpg 2x"
-            width="1280"
-            height="280"
-            alt="баннер"
-          />
-        </picture>
-        <p className="banner__info">
-          <span className="banner__message">Новинка!</span>
-          <span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span>
-          <span className="banner__text">
-            Профессиональная камера от&nbsp;известного производителя
-          </span>
-          <a className="btn" href="/">
-            Подробнее
-          </a>
-        </p>
-      </div>
+      <Banner />
       <div className="page-content">
         <Breadcrumbs />
         <section className="catalog">
