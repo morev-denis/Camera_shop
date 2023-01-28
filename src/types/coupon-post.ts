a5d5db0e1f1a164e678cf96ee914d79d28aaf4dc
Denis Morev <morev.denis@gmail.com>
2023-01-28 17:08:44 +0300

    создать типы структур данных

---
+++ src/types/coupon-post.ts
@@ -0,0 +1,3 @@
+export type CouponPost = {
+  coupon: 'camera-333' | 'camera-444' | 'camera-555';
+};
---
+++ src/types/order-post.ts
@@ -0,0 +1,4 @@
+export type OrderPost = {
+  camerasIds: number[];
+  coupon: 'camera-333' | 'camera-444' | 'camera-555' | null;
+};
