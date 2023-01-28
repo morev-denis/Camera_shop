7419eb8280ec6d03ec84f86e920d59b3bd9fd5c9
Denis Morev <morev.denis@gmail.com>
2023-01-28 11:33:04 +0300

    Создать каталог CatalogSort

--- src/pages/catalog-screen/catalog-screen.tsx
+++ src/pages/catalog-screen/catalog-screen.tsx
@@ -1,5 +1,6 @@
 import Header from '../../components/header/header';
 import CatalogFilter from '../../components/catalog-filter/catalog-filter';
+import CatalogSort from '../../components/catalog-sort/catalog-sort';
 import Footer from '../../components/footer/footer';
 
 const CatalogScreen = () => (
@@ -58,46 +59,7 @@ const CatalogScreen = () => (
                 <CatalogFilter />
               </div>
               <div className="catalog__content">
-                <div className="catalog-sort">
-                  <form action="#">
-                    <div className="catalog-sort__inner">
-                      <p className="title title--h5">Сортировать:</p>
-                      <div className="catalog-sort__type">
-                        <div className="catalog-sort__btn-text">
-                          <input type="radio" id="sortPrice" name="sort" />
-                          <label htmlFor="sortPrice">по цене</label>
-                        </div>
-                        <div className="catalog-sort__btn-text">
-                          <input type="radio" id="sortPopular" name="sort" />
-                          <label htmlFor="sortPopular">по популярности</label>
-                        </div>
-                      </div>
-                      <div className="catalog-sort__order">
-                        <div className="catalog-sort__btn catalog-sort__btn--up">
-                          <input
-                            type="radio"
-                            id="up"
-                            name="sort-icon"
-                            aria-label="По возрастанию"
-                          />
-                          <label htmlFor="up">
-                            <svg width="16" height="14" aria-hidden="true">
-                              <use xlinkHref="#icon-sort"></use>
-                            </svg>
-                          </label>
-                        </div>
-                        <div className="catalog-sort__btn catalog-sort__btn--down">
-                          <input type="radio" id="down" name="sort-icon" aria-label="По убыванию" />
-                          <label htmlFor="down">
-                            <svg width="16" height="14" aria-hidden="true">
-                              <use xlinkHref="#icon-sort"></use>
-                            </svg>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
+                <CatalogSort />
                 <div className="cards catalog__cards">
                   <div className="product-card">
                     <div className="product-card__img">
