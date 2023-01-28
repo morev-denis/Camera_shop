a993d5e2379d22036294be6e306e410b39b2999b
Denis Morev <morev.denis@gmail.com>
2023-01-28 11:39:45 +0300

    создать компонент Breadcrumbs

--- src/pages/catalog-screen/catalog-screen.tsx
+++ src/pages/catalog-screen/catalog-screen.tsx
@@ -1,4 +1,5 @@
 import Header from '../../components/header/header';
+import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import CatalogFilter from '../../components/catalog-filter/catalog-filter';
 import CatalogSort from '../../components/catalog-sort/catalog-sort';
 import Footer from '../../components/footer/footer';
@@ -34,23 +35,7 @@ const CatalogScreen = () => (
         </p>
       </div>
       <div className="page-content">
-        <div className="breadcrumbs">
-          <div className="container">
-            <ul className="breadcrumbs__list">
-              <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link" href="index.html">
-                  Главная
-                  <svg width="5" height="8" aria-hidden="true">
-                    <use xlinkHref="#icon-arrow-mini"></use>
-                  </svg>
-                </a>
-              </li>
-              <li className="breadcrumbs__item">
-                <span className="breadcrumbs__link breadcrumbs__link--active">Каталог</span>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Breadcrumbs />
         <section className="catalog">
           <div className="container">
             <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
