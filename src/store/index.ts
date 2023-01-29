5a4ebc76df537f159a9623d8b308464b747e3f4b
Denis Morev <morev.denis@gmail.com>
2023-01-29 11:39:38 +0300

    настроить middleware thunk и передать в extraArgument экземпляр axios

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,4 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { reducer } from './reducer';
 
-export const store = configureStore({ reducer });
+import { createApi } from '../services/api';
+
+const api = createApi();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: { extraArgument: api } }),
+});
