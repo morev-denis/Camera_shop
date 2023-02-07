c4afcf89d7c1089d03bdde3876bb3d28178ca815
Denis Morev <morev.denis@gmail.com>
2023-02-07 22:12:19 +0300

    добавить тесты для асинхронных операций

---
+++ src/store/api-actions.test.ts
@@ -0,0 +1,148 @@
+import { Action } from 'redux';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import MockAdapter from 'axios-mock-adapter';
+import { configureMockStore } from '@jedmao/redux-mock-store';
+
+import { createApi } from '../services/api';
+
+import { State } from '../types/store';
+
+import {
+  makeFakeCamera,
+  makeFakeCameras,
+  makeFakePromo,
+  makeFakeSimilarCameras,
+  makeFakeReviews,
+  makeFakeReview
+} from '../utils/mocks';
+
+import {
+  fetchCamerasAction,
+  fetchCameraAction,
+  fetchPromoAction,
+  fetchSimilarCamerasAction,
+  fetchReviewsAction,
+  postReviewAction
+} from './api-actions';
+
+import { APIRoute } from '../const';
+
+describe('Async actions', () => {
+  const api = createApi();
+  const mockApi = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+
+  const mockStore = configureMockStore<
+    State,
+    Action<string>,
+    ThunkDispatch<State, typeof api, Action>
+  >(middlewares);
+
+  it('should dispatch Load_Promo when GET /promo', async () => {
+    const promo = makeFakePromo();
+
+    mockApi.onGet(APIRoute.Promo).reply(200, promo);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchPromoAction());
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      fetchPromoAction.pending.type,
+      'loadPromo',
+      fetchPromoAction.fulfilled.type,
+    ]);
+  });
+
+  it('should dispatch Load_Cameras when GET /cameras', async () => {
+    const cameras = makeFakeCameras();
+
+    mockApi.onGet(APIRoute.Cameras).reply(200, cameras);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchCamerasAction());
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      fetchCamerasAction.pending.type,
+      'loadCameras',
+      fetchCamerasAction.fulfilled.type,
+    ]);
+  });
+
+  it('should dispatch Load_Camera when GET /cameras/1', async () => {
+    const camera = makeFakeCamera();
+
+    mockApi.onGet(`${APIRoute.Cameras}/1`).reply(200, camera);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchCameraAction(1));
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      fetchCameraAction.pending.type,
+      'loadCamera',
+      fetchCameraAction.fulfilled.type,
+    ]);
+  });
+
+  it('should dispatch Load_SimilarCameras when GET /cameras/1/similar', async () => {
+    const similarCameras = makeFakeSimilarCameras();
+
+    mockApi.onGet(`${APIRoute.Cameras}/1/similar`).reply(200, similarCameras);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchSimilarCamerasAction(1));
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      fetchSimilarCamerasAction.pending.type,
+      'loadSimilarCameras',
+      fetchSimilarCamerasAction.fulfilled.type,
+    ]);
+  });
+
+  it('should dispatch Load_Reviews when GET /cameras/1/reviews', async () => {
+    const reviews = makeFakeReviews();
+
+    mockApi.onGet(`${APIRoute.Cameras}/1/reviews`).reply(200, reviews);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchReviewsAction(1));
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      fetchReviewsAction.pending.type,
+      'loadReviews',
+      fetchReviewsAction.fulfilled.type,
+    ]);
+  });
+
+  it('should dispatch Post_Review when POST /reviews', async () => {
+    const review = makeFakeReview();
+
+    mockApi.onPost(APIRoute.Reviews).reply(200, review);
+
+    const store = mockStore();
+
+    await store.dispatch(postReviewAction(review));
+
+    const actions = store.getActions().map(({ type }) => type);
+
+    expect(actions).toEqual([
+      postReviewAction.pending.type,
+      'postReview',
+      postReviewAction.fulfilled.type,
+    ]);
+  });
+});
--- src/store/reducer.test.ts
+++ src/store/reducer.test.ts
@@ -2,16 +2,6 @@ import { reducer } from './reducer';
 
 import { REVIEWS_COUNT } from '../const';
 
-import { increaseReviewsCount } from './action';
-
-import {
-  fetchCamerasAction,
-  fetchCameraAction,
-  fetchPromoAction,
-  fetchSimilarCamerasAction,
-  fetchReviewsAction,
-} from './api-actions';
-
 import {
   makeFakeCamera,
   makeFakeCameras,
@@ -43,44 +33,42 @@ describe('Reducer', () => {
   });
 
   it('should increase reviews count', () => {
-    expect(reducer(initialState, { type: increaseReviewsCount })).toEqual({
+    expect(reducer(initialState, { type: 'increaseReviewsCount' })).toEqual({
       ...initialState,
       reviewsCount: initialState.reviewsCount + REVIEWS_COUNT,
     });
   });
 
   it('should update camera by load camera', () => {
-    expect(reducer(initialState, { type: fetchCameraAction, payload: camera })).toEqual({
+    expect(reducer(initialState, { type: 'loadCamera', payload: camera })).toEqual({
       ...initialState,
       camera,
     });
   });
 
   it('should update cameras by load cameras', () => {
-    expect(reducer(initialState, { type: fetchCamerasAction, payload: cameras })).toEqual({
+    expect(reducer(initialState, { type: 'loadCameras', payload: cameras })).toEqual({
       ...initialState,
       cameras,
     });
   });
 
   it('should update promo by load promo', () => {
-    expect(reducer(initialState, { type: fetchPromoAction, payload: promo })).toEqual({
+    expect(reducer(initialState, { type: 'loadPromo', payload: promo })).toEqual({
       ...initialState,
       promo,
     });
   });
 
   it('should update similar cameras by load similar cameras', () => {
-    expect(
-      reducer(initialState, { type: fetchSimilarCamerasAction, payload: similarCameras }),
-    ).toEqual({
+    expect(reducer(initialState, { type: 'loadSimilarCameras', payload: similarCameras })).toEqual({
       ...initialState,
       similarCameras,
     });
   });
 
   it('should update review by load reviews', () => {
-    expect(reducer(initialState, { type: fetchReviewsAction, payload: reviews })).toEqual({
+    expect(reducer(initialState, { type: 'loadReviews', payload: reviews })).toEqual({
       ...initialState,
       reviews,
     });
